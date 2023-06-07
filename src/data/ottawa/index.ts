9c6754db89fc6118a4b1c4948303e384fa35367c
nicklewanowicz <nicklewanowicz@gmail.com>
2023-06-07 08:51:18 -0400

    types

--- src/data/ottawa/index.ts
+++ src/data/ottawa/index.ts
@@ -1,8 +1,8 @@
-import data_2019 from './2019';
-import data_2020 from './2020';
-import data_2021 from './2021';
-import data_2022 from './2022';
-import { DailyWeather } from '../types';
+import data_2019 from './2019'
+import data_2020 from './2020'
+import data_2021 from './2021'
+import data_2022 from './2022'
+import { DailyWeather } from '../'
 
 const data: DailyWeather[] = [
   ...data_2019,
@@ -15,6 +15,6 @@ const data: DailyWeather[] = [
     datetime: new Date(day.datetime),
     windgust: Number(day.windgust),
     severerisk: Number(day.severerisk),
-  };
-});
-export default data;
+  }
+})
+export default data
