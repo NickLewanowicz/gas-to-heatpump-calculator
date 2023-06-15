d3053d00de7479df78cb0679d32b03a4428cb15b
NickLewanowicz <NickLewanowicz@users.noreply.github.com>
2023-06-15 07:45:19 -0400

    Add 2 year toronto data

---
+++ src/data/toronto/index.ts
@@ -0,0 +1,15 @@
+// import data_2019 from './2019'
+// import data_2020 from './2020'
+import data_2021 from './2021';
+import data_2022 from './2022';
+import { DailyWeather } from '../';
+
+const data: DailyWeather[] = [...data_2021, ...data_2022].map((day) => {
+  return {
+    ...day,
+    datetime: new Date(day.datetime),
+    windgust: Number(day.windgust),
+    severerisk: Number(day.severerisk),
+  };
+});
+export default data;
