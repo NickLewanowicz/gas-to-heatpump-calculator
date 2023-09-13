ecc784c0ac411243dc363ffd247ec73fdbecf04b
NickLewanowicz <NickLewanowicz@users.noreply.github.com>
2023-09-12 22:44:30 -0400

    3 files modified, 1 added

--- src/data/weather/toronto/index.ts
+++ src/data/weather/toronto/index.ts
@@ -1,15 +1,15 @@
 // import data_2019 from './2019'
-// import data_2020 from './2020'
+import data_2020 from './2020';
 import data_2021 from './2021';
 import data_2022 from './2022';
-import { DailyWeather } from '../';
+import { HourlyWeather } from '../';
 
-const data: DailyWeather[] = [...data_2021, ...data_2022].map((day) => {
-  return {
-    ...day,
-    datetime: new Date(day.datetime),
-    windgust: Number(day.windgust),
-    severerisk: Number(day.severerisk),
-  };
-});
+const data: HourlyWeather[] = [...data_2020, ...data_2021, ...data_2022].map(
+  (day) => {
+    return {
+      datetime: new Date(day.time),
+      temp: Number(day['temperature_2m (°C)']),
+    };
+  }
+);
 export default data;
