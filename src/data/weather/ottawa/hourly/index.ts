85148c87b2bbe526e8ee10d76b15276f6276d9ac
nicklewanowicz <nicklewanowicz@gmail.com>
2023-09-18 21:29:39 -0400

    fix

--- src/data/weather/ottawa/hourly/index.ts
+++ src/data/weather/ottawa/hourly/index.ts
@@ -1,11 +1,11 @@
 
 
-import { HourlyWeather } from '../../'
+// import { HourlyWeather } from '../../'
 
-const data: HourlyWeather[] = [...data_2022].map((hour) => {
-  return {
-    datetime: new Date(hour.time),
-    temp: hour['temperature_2m (°C)'],
-  }
-})
-export default data
+// const data: HourlyWeather[] = [...data_2022].map((hour) => {
+//   return {
+//     datetime: new Date(hour.time),
+//     temp: hour['temperature_2m (°C)'],
+//   }
+// })
+// export default data
